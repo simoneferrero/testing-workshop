74366299bc55473bdb44fe258af7b6d65a63cff3
Simone Ferrero <simone.ferrero@gamesys.co.uk>
2019-12-18 11:42:20 +0000

    Refactored class to function with hooks

--- src/components/Todos/index.js
+++ src/components/Todos/index.js
@@ -1,62 +1,45 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import Todo from './Todo'
 import TodoForm from './TodoForm'
 import { StyledLabel, StyledPaper, StyledWrapper } from './styled'
 
-class Todos extends React.Component {
-	constructor() {
-		super()
-
-		this.handleClick = this.handleClick.bind(this)
-		this.handleSubmit = this.handleSubmit.bind(this)
-	}
-
-	state = {
-		showCompleted: true,
-		todos: [],
-	}
-
-	async handleClick(id, completed) {
-		this.setState(({ todos }) => ({
-			todos: todos.map((todo) =>
-				todo.id === id ? { ...todo, completed } : todo
-			),
-		}))
-	}
-
-	async handleSubmit(text) {
+const Todos = () => {
+	const [showCompleted, setShowCompleted] = useState(true)
+	const [todos, setTodos] = useState([])
+	const handleSubmit = async (text) => {
 		const todo = {
 			id: String(Date.now()),
 			text,
 			completed: false,
 		}
-		this.setState(({ todos }) => ({ todos: [...todos, todo] }))
+		setTodos([...todos, todo])
+	}
+	const handleClick = async (id, completed) => {
+		setTodos(todos.map((todo) =>
+			todo.id === id ? { ...todo, completed } : todo
+		))
 	}
 
-	render() {
-		const { showCompleted, todos } = this.state
-
-		return (
-			<StyledWrapper data-testid="todos">
-				<TodoForm handleSubmit={this.handleSubmit} />
-				<StyledLabel htmlFor="showCompleted">
-					Show Completed:{' '}
-					<input
-						checked={showCompleted}
-						id="showCompleted"
-						onChange={() => this.setState({ showCompleted: !showCompleted })}
-						type="checkbox"
-					/>
-				</StyledLabel>
-				<StyledPaper data-testid="paper">
-					{todos.map((todo) => (
-						<Todo handleClick={this.handleClick} key={todo.id} {...todo} />
-					))}
-				</StyledPaper>
-			</StyledWrapper>
-		)
-	}
+	return (
+		<StyledWrapper data-testid="todos">
+			<TodoForm handleSubmit={ handleSubmit } />
+			<StyledLabel htmlFor="showCompleted">
+				Show Completed:{ ' ' }
+				<input
+					checked={ showCompleted }
+					id="showCompleted"
+					onChange={ () => setShowCompleted(!showCompleted) }
+					type="checkbox"
+				/>
+			</StyledLabel>
+			<StyledPaper data-testid="paper">
+				{ todos.map((todo) => (
+					<Todo handleClick={ handleClick } key={ todo.id } { ...todo } />
+				)) }
+			</StyledPaper>
+		</StyledWrapper>
+	)
 }
 
 export default Todos
